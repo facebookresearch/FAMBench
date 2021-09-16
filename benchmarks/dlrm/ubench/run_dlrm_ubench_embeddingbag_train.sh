06ba8773cca7751bf0c3d157f430623d019a5cca
Satish Nadathur <nrsatish@fb.com>
2021-09-16 10:23:31 -0700

    Tweaked file path names and added comms bench

---
+++ benchmarks/dlrm/ubench/run_dlrm_ubench_embeddingbag_train.sh
@@ -0,0 +1,16 @@
+steps=100
+device='cpu'
+dataset='A'
+
+usage() { echo "Usage: $0 [-s <steps>] [-d <'cpu'|'gpu'>]"; exit 1; }
+
+while getopts "s:d:" flag
+do
+    case "${flag}" in
+        s) steps=${OPTARG};;
+        d) device=${OPTARG};;
+    esac
+done
+shift $((OPTIND-1))
+
+python ../../../../param/train/compute/pt/driver.py --steps=$steps --device=$device emb --dataset='A'
