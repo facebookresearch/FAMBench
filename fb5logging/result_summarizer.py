dd12e0332884f7b757decb4f479c0f974299f0be
Eric Han <erichan1@fb.com>
2021-09-20 19:59:34 +0000

    summarizer summarizes to file with flat table structure

--- fb5logging/result_summarizer.py
+++ fb5logging/result_summarizer.py
@@ -9,7 +9,6 @@ import re
 import sys
 import glob
 
-
 _HEADER_REGEX = r'.*"key": "header".*'
 _RUN_START_REGEX = r'.*"key": "run_start".*'
 _RUN_STOP_REGEX = r'.*"key": "run_stop".*'
@@ -20,14 +19,14 @@ def _find_and_read_row(result : str, regex : str):
     """
     row = re.search(regex, result)
     if row is None:
-      raise Exception('Failed to match regex!')
+      raise Exception('Failed to match regex: '.format(regex))
     row = json.loads(row.group(0))
     row.pop('key')
     return row
 
 def _calculate_metrics(result : str):
     """
-    Calculates runtime in seconds, num_batches
+    Calculates the main score for the benchmark run and other metrics/metadata. 
     """
     run_start_row = _find_and_read_row(result, _RUN_START_REGEX)
     run_stop_row = _find_and_read_row(result, _RUN_STOP_REGEX)
@@ -43,10 +42,22 @@ def _calculate_metrics(result : str):
     average_batch_time = seconds_runtime / num_batches
     result = {'score' : throughput, 'num_batches' : num_batches, 'batch_size' : batch_size, 'average_batch_time': average_batch_time}
 
-    #append extra_metadaata if present
+    #append extra_metadata if present
     if 'extra_metadata' in run_stop_row:
         result['extra_metadata'] = run_stop_row['extra_metadata']
     return result
+
+def _flatten_dict(d: dict):
+    """
+    Flattens a nested dictionary, not in-place
+    """
+    res = {}
+    for key, val in d.items():
+        if isinstance(val, dict):
+            res.update(_flatten_dict(val))
+        else:
+            res[key] = val
+    return res
 
 def _create_summary_row(file_path : str):
     """
@@ -57,8 +68,43 @@ def _create_summary_row(file_path : str):
         result = f.read()
 
     row = _find_and_read_row(result, _HEADER_REGEX)
-    row['results'] = _calculate_metrics(result)
+    results = _calculate_metrics(result)
+    row['results'] = results
     return row
+
+def _lst_to_file(lst: list, file_path):
+    for i in range(len(lst)):
+        lst[i] = str(lst[i])
+    delimiter = ' ' #space delimiter
+    with open(file_path, 'a') as f:
+        f.write(delimiter.join(lst) + '\n')
+
+def _rows_to_file(rows: list[dict], folder_path: str):
+    """
+    Save list of summary rows into a human-readable table in a file
+    """
+    file_path = folder_path + '/summary.txt'
+
+    if(len(rows) == 0):
+        return
+
+    all_keys = _flatten_dict(rows[0]).keys()
+    top_level_keys = [
+        "benchmark",
+        "implementation",
+        "mode",
+        "config",
+        "score"]
+    other_keys = [k for k in all_keys if k not in top_level_keys]
+    keys_in_order = top_level_keys + other_keys
+    _lst_to_file(keys_in_order, file_path)
+    for row in rows:
+        flattened_row = _flatten_dict(row)
+        top_val_lst = [flattened_row[k] for k in top_level_keys]
+        other_val_lst = [flattened_row[k] for k in other_keys]
+        combined_lst = top_val_lst + other_val_lst
+        _lst_to_file(combined_lst, file_path)
+        
 
 def summarize_results(benchmark_folder):
     """Summarizes a set of results.
@@ -78,9 +124,9 @@ def summarize_results(benchmark_folder):
         rows.append(row)
 
     for row in rows:
-        print(row) # TODO log into a file in the same folder as benchmark_folder. Have it overwrite the file too.
+        print(row) 
 
-
+    return rows
 
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
@@ -92,4 +138,5 @@ def init_argparse() -> argparse.ArgumentParser:
 if __name__ == '__main__':
     parser = init_argparse()
     args = parser.parse_args()
-    summarize_results(args.benchmark_folder)
+    rows = summarize_results(args.benchmark_folder)
+    _rows_to_file(rows, args.benchmark_folder)
