b35adb17ffaf249f657721a8d90ca6e8e4bb749a
erichan1 <30481032+erichan1@users.noreply.github.com>
2021-09-27 14:54:13 -0700

    added timing as a param to fb5logger (#14)
    * added timing as a param to fb5logger
    
    * add period to respond to comment

--- fb5logging/fb5logger.py
+++ fb5logging/fb5logger.py
@@ -21,17 +21,30 @@ class FB5Logger():
             f.write('\n')
 
     def _time_ms(self):
+        """
+        Naive implementation of current time.
+        """
         return round(time.time() * 1000)
 
     def header(self, benchmark_name, implementation_name, mode, config_name):
         header_dict = {"benchmark": benchmark_name, "implementation": implementation_name, "mode": mode, "config": config_name, "key": "header"}
         self._dump_json(header_dict)
 
-    def run_start(self):
-        start_dict = {"time_ms": self._time_ms(), "key": "run_start"}
+    def run_start(self, time_ms = None):
+        """
+        Records start of logging.
+        """
+        if(time_ms is None):
+            time_ms = self._time_ms()
+        start_dict = {"time_ms": time_ms, "key": "run_start"}
         self._dump_json(start_dict)
 
-    def run_stop(self, num_batches, batch_size, extra_metadata = None):
+    def run_stop(self, num_batches, batch_size, extra_metadata = None, time_ms = None):
+        """
+        Records end of logging and any required data. 
+        """
+        if(time_ms is None):
+            time_ms = self._time_ms()
         start_dict = {"time_ms": self._time_ms(), "num_batches": num_batches, "batch_size": batch_size, "key": "run_stop"}
         if extra_metadata is not None:
             start_dict["extra_metadata"] = extra_metadata
