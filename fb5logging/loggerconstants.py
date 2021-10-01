2de650de7faa0c810d05065f1c5bbc672ccf2fc0
erichan1 <30481032+erichan1@users.noreply.github.com>
2021-10-01 14:58:20 -0700

    added loggerconstants to make logging more flexible (#15)

---
+++ fb5logging/loggerconstants.py
@@ -0,0 +1,20 @@
+"""
+Master list of constants (log keys) for logger
+"""
+
+# header
+HEADER = "header"
+
+# timing info
+EPOCH_START = "epoch_start"
+EPOCH_STOP = "epoch_stop"
+RUN_START = "run_start"
+RUN_STOP = "run_stop"
+
+# run information
+NUM_BATCHES = "num_batches"
+BATCH_SIZE = "batch_size"
+FLOPS = "flops"
+
+# model hyperparameters
+LEARNING_RATE = "learning_rate"
