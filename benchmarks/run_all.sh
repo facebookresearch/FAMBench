929c63f91695bab9d0a7b68fd34ec4b7787589b4
Eric Han <erichan1@fb.com>
2021-10-05 23:32:50 +0000

    cleaned up run_all script

--- benchmarks/run_all.sh
+++ benchmarks/run_all.sh
@@ -3,8 +3,7 @@
 # DLRM
 ./run_dlrm_ootb_infer.sh -l results
 ./run_dlrm_ootb_train.sh -l results
-./run_dlrm_ubench_train_embeddingbag.sh -l results
-./run_dlrm_ubench_train_linear.sh -l results
+./run_dlrm_ubench_train_linear.sh -c "[(2,2,2,2,2)]" -l results # Config not real
 
 # view options: [raw_view, intermediate_view]
 python ../fb5logging/result_summarizer.py -f results -v intermediate_view
