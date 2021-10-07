967cdc3cdd7085121bfacbc117b54ccf0f4c63d4
Eric Han <erichan1@fb.com>
2021-10-07 23:21:20 +0000

    added more desc to run_all.sh

--- benchmarks/run_all.sh
+++ benchmarks/run_all.sh
@@ -8,5 +8,6 @@
 ./run_dlrm_ubench_train_linear.sh -c "[(2,2,2,2,2)]" -l results # Config not real
 ./run_dlrm_ubench_train_embeddingbag.sh -l results -c "[(2,2,2,2),(2,2,2,2),(2,2,2,2),(2,2,2,2),(2,2,2,2)]" # Config not real
 
-# view options: [raw_view, intermediate_view]
+# view options: [raw_view -> pure json, intermediate_view -> nice table]
+# intermediate view recommended for filling out table
 python ../fb5logging/result_summarizer.py -f results -v intermediate_view
