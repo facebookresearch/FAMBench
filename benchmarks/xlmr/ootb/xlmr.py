6fafce0266703c0491d81843281f48fadf45eb4f
Eric Han <erichan1@fb.com>
2021-10-13 22:31:02 +0000

    move unneeded functions to xlmr_extra

--- benchmarks/xlmr/ootb/xlmr.py
+++ benchmarks/xlmr/ootb/xlmr.py
@@ -17,12 +17,6 @@ def get_inference_model():
     # TODO use torchscript? jit/script this model?
     return fairseq_xlmr_large.model 
 
-def get_inference_data():
-    test_dp = PennTreebank(split='test')
-    # TODO prepare this data properly 
-
-    return test_dp
-
 def generate_inference_data(nbatches=10, batchsize=32, seq_length=64, vocab_size=1000):
     shape = (nbatches, batchsize, seq_length)
     data = torch.rand(shape) * vocab_size
@@ -35,27 +29,6 @@ def evaluate_simple(model, input_data):
     """
     for batch in input_data:
         output = model(batch)
-
-def evaluate(test_dp, model):
-    """
-    evaluation loop for xlmr
-    """
-    model.eval() 
-    total_correct, total_count = 0.0, 0.0
-    with torch.no_grad():
-        for batch in test_dp:
-            print(batch)
-            model_input = batch["pad_token_ids"] # TODO .to(device). same for next line
-            target = torch.tensor(batch["labels"])
-            logits = model(model_input)
-            correct = (logits.argmax(1) == target).sum()
-            total_correct+=float(correct)
-            total_count+=float(target.size(0))
-    return total_correct/total_count
-
-def train(self, niter=1):
-    # TODO need the right loss, correct optimizer/learning rate, etc
-    pass
 
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
