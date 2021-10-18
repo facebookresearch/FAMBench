2bcb80a2c991c3ef896bd60ae8a7aa2dbd90da17
Eric Han <erichan1@fb.com>
2021-10-18 22:17:22 +0000

    ported xlmr to use gpu

--- benchmarks/xlmr/ootb/xlmr.py
+++ benchmarks/xlmr/ootb/xlmr.py
@@ -9,6 +9,14 @@ p = pathlib.Path(__file__).parent.resolve() / "../../../fb5logging"
 sys.path.append(fspath(p))
 from fb5logger import FB5Logger
 
+def time_sec(use_gpu):
+    """
+    Return time. If gpu is available, synchronize. 
+    """
+    if use_gpu:
+        torch.cuda.synchronize()
+    return time.time()
+
 def get_inference_model():
     fairseq_xlmr_large = torch.hub.load('pytorch/fairseq:main', 'xlmr.large') 
     fairseq_xlmr_large.eval()
@@ -37,11 +45,17 @@ def init_argparse() -> argparse.ArgumentParser:
     parser.add_argument("--logfile", type=str, default=None)
     parser.add_argument("--inference-only", action="store_true", default=False)
     parser.add_argument("--fb5config", type=str, default="tiny")
+    parser.add_argument("--use-gpu", action="store_true", default=True) 
     return parser
 
 def run():
     parser = init_argparse()
     args = parser.parse_args()
+
+    # check for device
+    if(args.use_gpu):
+        assert torch.cuda.is_available(), "No cuda device is available."
+        device = torch.device("cuda", 0)
 
     # prep logger
     if args.logfile is not None:
@@ -60,17 +74,21 @@ def run():
     else:
         pass # TODO train side
 
-    # TODO: run timer sync for gpu torch.cuda.synchronize(). run .cuda() on model and data here. 
-    # Use .half() on the model to use fp16 instead of 32. 2x size!
+    # use gpu
+    if args.use_gpu: 
+        data = data.cuda()
+        xlmr = xlmr.cuda()
 
     # benchmark! 
     if args.logfile is not None:
-        famlogger.run_start()
+        t_start_ms = time_sec(args.use_gpu) * 1000
+        famlogger.run_start(time_ms=t_start_ms)
 
     evaluate_simple(xlmr, data, famlogger=famlogger) 
 
     if args.logfile is not None:
-        famlogger.run_stop(1, 1)    
+        t_stop_ms = time_sec(args.use_gpu) * 1000
+        famlogger.run_stop(1, 1, time_ms=t_stop_ms) # run_stop batch info to be deprecated, so put in meaningless args    
         famlogger.record_batch_info(num_batches=data.shape[0], batch_size=data.shape[1])
 
 if __name__ == "__main__":
