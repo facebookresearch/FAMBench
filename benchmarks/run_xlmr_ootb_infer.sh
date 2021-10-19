41295b9c20f4d13456238cd37800696527bb1ce9
Eric Han <erichan1@fb.com>
2021-10-19 16:18:48 +0000

    port xlmr to use gpu

--- benchmarks/run_xlmr_ootb_infer.sh
+++ benchmarks/run_xlmr_ootb_infer.sh
@@ -8,4 +8,4 @@ config=tiny # default is tiny, proof of concept
 LOG_DIR=results
 LOGGER_FILE="${LOG_DIR}/${benchmark}_${implementation}_${mode}_${config}.log"
 
-python "${benchmark}/${implementation}/xlmr.py" --inference-only ${config_flags} --fb5logger=${LOGGER_FILE} --fb5config=${config}
+python "${benchmark}/${implementation}/xlmr.py" --inference-only ${config_flags} --logfile=${LOGGER_FILE} --fb5config=${config} --use-gpu
--- benchmarks/xlmr/ootb/xlmr.py
+++ benchmarks/xlmr/ootb/xlmr.py
@@ -9,6 +9,14 @@ p = pathlib.Path(__file__).parent.resolve() / "../../../fb5logging"
 sys.path.append(fspath(p))
 from fb5logger import FB5Logger
 
+def time_sec(use_gpu):
+    """
+    Return time. If gpu is available, synchronize. 
+    """
+    if use_gpu:
+        torch.cuda.synchronize()
+    return time.time()
+
 def get_inference_model():
     fairseq_xlmr_large = torch.hub.load('pytorch/fairseq:main', 'xlmr.large') 
     fairseq_xlmr_large.eval()
@@ -21,52 +29,67 @@ def generate_inference_data(nbatches=10, batchsize=32, seq_length=64, vocab_size
     data = data.int()
     return data
     
-def evaluate_simple(model, input_data):
+def evaluate_simple(model, input_data, famlogger=None):
     """
     Run data through the model
     """
     for batch in input_data:
+        # famlogger.batch_start()
         output = model(batch)
+        # famlogger.batch_stop()
 
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description="Benchmark XLM-R model"
     )
-    parser.add_argument("--fb5logger", type=str, default=None)
+    parser.add_argument("--logfile", type=str, default=None)
     parser.add_argument("--inference-only", action="store_true", default=False)
     parser.add_argument("--fb5config", type=str, default="tiny")
+    parser.add_argument("--use-gpu", action="store_true", default=False) 
     return parser
 
 def run():
     parser = init_argparse()
     args = parser.parse_args()
 
+    # check for device
+    if(args.use_gpu):
+        assert torch.cuda.is_available(), "No cuda device is available."
+        device = torch.device("cuda", 0)
+
     # prep logger
-    if args.fb5logger is not None:
-        fb5logger = FB5Logger(args.fb5logger)
+    if args.logfile is not None:
+        famlogger = FB5Logger(args.logfile)
         if(args.inference_only): 
-            fb5logger.header("XLMR", "OOTB", "eval", args.fb5config)
+            famlogger.header("XLMR", "OOTB", "eval", args.fb5config)
         else:
-            fb5logger.header("XLMR", "OOTB", "train", args.fb5config)
+            famlogger.header("XLMR", "OOTB", "train", args.fb5config)
             
     # prep model and data
     xlmr = None
     data = None
     if(args.inference_only): 
-        data = generate_inference_data()
+        data = generate_inference_data(nbatches=10)
         xlmr = get_inference_model()
     else:
         pass # TODO train side
 
+    # use gpu
+    if args.use_gpu: 
+        data = data.to(device)
+        xlmr = xlmr.to(device)
+
     # benchmark! 
-    if args.fb5logger is not None:
-        fb5logger.run_start()
+    if args.logfile is not None:
+        t_start_ms = time_sec(args.use_gpu) * 1000
+        famlogger.run_start(time_ms=t_start_ms)
 
-    evaluate_simple(xlmr, data) 
+    evaluate_simple(xlmr, data, famlogger=famlogger) 
 
-    nbatches, batch_size = data.shape[0], data.shape[1]
-    if args.fb5logger is not None:
-        fb5logger.run_stop(nbatches, batch_size)    
+    if args.logfile is not None:
+        t_stop_ms = time_sec(args.use_gpu) * 1000
+        famlogger.run_stop(data.shape[0], data.shape[1], time_ms=t_stop_ms)   
+        # famlogger.record_batch_info(num_batches=data.shape[0], batch_size=data.shape[1])
 
 if __name__ == "__main__":
     run()
