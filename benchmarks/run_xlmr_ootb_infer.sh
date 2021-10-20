6ff8378619e1b823410d3e9d7e82bd32d4fd611f
Eric Han <erichan1@fb.com>
2021-10-20 22:51:28 +0000

    fix unsaved merge conflict resolve

--- benchmarks/run_xlmr_ootb_infer.sh
+++ benchmarks/run_xlmr_ootb_infer.sh
@@ -8,8 +8,4 @@ config=fb-1dev-short
 LOG_DIR=results
 LOGGER_FILE="${LOG_DIR}/${benchmark}_${implementation}_${mode}_${config}.log"
 
-<<<<<<< HEAD
-python "${benchmark}/${implementation}/xlmr.py" --inference-only ${config_flags} --logfile=${LOGGER_FILE} --fb5config=${config}
-=======
 python "${benchmark}/${implementation}/xlmr.py" --inference-only ${config_flags} --logfile=${LOGGER_FILE} --famconfig=${config} --use-gpu
->>>>>>> 74dc5d29c238c013643fdcc62d7a851a02467eb3
