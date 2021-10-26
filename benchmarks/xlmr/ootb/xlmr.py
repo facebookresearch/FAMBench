254275f6fad9287ab5e5ef14c91f1c44e570344a
Sami Wilf <samiw1@fb.com>
2021-10-26 10:25:57 -0700

    Add simple training loop. Include cpu-to-gpu ml data transfer in benchmark measurement

--- benchmarks/xlmr/ootb/xlmr.py
+++ benchmarks/xlmr/ootb/xlmr.py
@@ -2,6 +2,7 @@ import torch
 import argparse
 import sys
 import time
+import torch.nn.functional as F
 
 # FB5 Logger
 import pathlib
@@ -10,34 +11,47 @@ p = pathlib.Path(__file__).parent.resolve() / "../../../fb5logging"
 sys.path.append(fspath(p))
 from fb5logger import FB5Logger
 
+from fairseq.models.roberta import XLMRModel
+
 def time_ms(use_gpu):
     """
-    Return time. If gpu is available, synchronize. 
+    Return time. If gpu is available, synchronize.
     """
     if use_gpu:
         torch.cuda.synchronize()
     return time.time_ns() * 1e-6
 
-def get_inference_model():
-    fairseq_xlmr_large = torch.hub.load('pytorch/fairseq:main', 'xlmr.large') 
-    fairseq_xlmr_large.eval()
-    fairseq_xlmr_large.half() # TODO make this a command line arg
-    # TODO use torchscript? jit/script this model?
-    return fairseq_xlmr_large.model 
+def get_model():
+    # download from Internet
+    fairseq_xlmr_large = torch.hub.load('pytorch/fairseq:main', 'xlmr.large')
 
-def generate_inference_data(nbatches=100, batchsize=64, seq_length=64, vocab_size=1000):
-    shape = (nbatches, batchsize, seq_length)
-    data = torch.rand(shape) * vocab_size
-    data = data.int()
-    return data
-    
-def evaluate_simple(model, input_data, famlogger=None):
+    # load model weights file locally
+    # f = '/path/xlmr.large'
+    # fairseq_xlmr_large = XLMRModel.from_pretrained(f, checkpoint_file='model.pt')
+
+    # TODO use torchscript? jit/script this model?
+    return fairseq_xlmr_large.model
+
+def generate_ml_sample(batchsize=64, seq_length=64, vocab_size=1000, get_y_true=True):
+    shape = (batchsize, seq_length)
+    x = torch.rand(shape) * vocab_size
+    x = x.int()
+    if get_y_true:
+        y_true = torch.rand((batchsize, seq_length, 250002))
+        return [x, y_true]
+    else:
+        return x
+
+def evaluate_simple(model, x_l, use_gpu=False, famlogger=None):
     """
     Run data through the model
     """
-    for batch in input_data:
+    for x in x_l:
         famlogger.batch_start()
-        output = model(batch)
+        if use_gpu:
+            x = x.cuda()
+        y_pred = model(x)
+        torch.cuda.synchronize()
         famlogger.batch_stop()
 
 def init_argparse() -> argparse.ArgumentParser:
@@ -47,7 +61,8 @@ def init_argparse() -> argparse.ArgumentParser:
     parser.add_argument("--logfile", type=str, default=None)
     parser.add_argument("--inference-only", action="store_true", default=False)
     parser.add_argument("--famconfig", type=str, default="tiny")
-    parser.add_argument("--use-gpu", action="store_true", default=False) 
+    parser.add_argument("--use-gpu", action="store_true", default=False)
+    parser.add_argument("--num-batches", type=int, default=5)
     return parser
 
 def run():
@@ -62,34 +77,51 @@ def run():
     # prep logger
     if args.logfile is not None:
         famlogger = FB5Logger(args.logfile)
-        if(args.inference_only): 
+        if(args.inference_only):
             famlogger.header("XLMR", "OOTB", "eval", args.famconfig)
         else:
             famlogger.header("XLMR", "OOTB", "train", args.famconfig)
-            
-    # prep model and data
-    xlmr = None
-    data = None
-    if(args.inference_only): 
-        data = generate_inference_data()
-        xlmr = get_inference_model()
-    else:
-        pass # TODO train side
 
+    # prep model and data
+    xlmr = get_model()
+    if args.inference_only:
+        xlmr.eval()
+    
     # use gpu
-    if args.use_gpu: 
-        data = data.to(device)
+    if args.use_gpu:
         xlmr = xlmr.to(device)
 
-    # benchmark! 
+    if args.inference_only:
+        x_l = [generate_ml_sample(get_y_true=False) for _ in range(args.num_batches)]
+    else:
+        x_l, y_true_l = zip(*[generate_ml_sample() for _ in range(args.num_batches)])
+
+    # benchmark!
     if args.logfile is not None:
         famlogger.run_start(time_ms=time_ms(args.use_gpu))
 
-    evaluate_simple(xlmr, data, famlogger=famlogger) 
+    if args.inference_only:
+        evaluate_simple(xlmr, x_l, args.use_gpu, famlogger=famlogger)
+    else:
+        #training loop
+        learning_rate = 0.01
+        optimizer = torch.optim.SGD(xlmr.parameters(), lr=learning_rate)
+        for x, y_true in zip(x_l, y_true_l):    
+            famlogger.batch_start()
+            if args.use_gpu:
+                x = x.to(device)
+                y_true = y_true.to(device)
+            y_pred = xlmr(x)
+            loss = F.cross_entropy(y_pred[0], y_true)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad() 
+            torch.cuda.synchronize()      
+            famlogger.batch_stop()
 
     if args.logfile is not None:
-        famlogger.run_stop(0, 0, time_ms=time_ms(args.use_gpu))    
-        famlogger.record_batch_info(num_batches=data.shape[0], batch_size=data.shape[1])
+        famlogger.run_stop(0, 0, time_ms=time_ms(args.use_gpu))
+        famlogger.record_batch_info(num_batches=len(x_l), batch_size=len(x_l[0]))
 
 if __name__ == "__main__":
     run()
