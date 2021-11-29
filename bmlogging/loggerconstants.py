8d62785b72e38118f37d65411deed2a402dec3fc
erichan1 <30481032+erichan1@users.noreply.github.com>
2021-11-29 10:19:39 -0800

    convert famlogger to bmlogger with nop (#55)
    * convert famlogger to bmlogger with nop
    
    * create new bmlogger folder as transition to new name
    
    * revert changes to fb5logger file bc bmlogger file created
    
    * minor bugfix
    
    * remove old test_mllog example file
    
    * Added XLMR warmup (#56)
    
    * added xlmr warmup
    
    * deduped train and inf loop and removed xlmr extra file
    
    * edited initial readme
    
    * minor fixes
    
    * fix zero warmup batches bug

---
+++ bmlogging/loggerconstants.py
@@ -0,0 +1,32 @@
+"""
+Master list of constants for logger
+Mostly logger keys, but some other constants as well.
+"""
+
+# loggerkey - header
+HEADER = "header"
+
+# loggerkey - timing info
+EPOCH_START = "epoch_start"
+EPOCH_STOP = "epoch_stop"
+RUN_START = "run_start"
+RUN_STOP = "run_stop"
+BATCH_START = "batch_start"
+BATCH_STOP = "batch_stop"
+
+# loggerkey - run information
+NUM_BATCHES = "num_batches"
+BATCH_SIZE = "batch_size"
+FLOPS = "flops"
+
+# loggerkey - model hyperparameters
+LEARNING_RATE = "learning_rate"
+
+# type of summary view saved to file
+INTERMEDIATE_VIEW = "intermediate_view" # table view
+RAW_VIEW = "raw_view" # json view
+
+# available types of score metrics
+EXPS = "exps" # examples/sec (throughput)
+TFPS = "tfps" # teraflops/sec (floating point ops rate)
+GBPS = "gbps" # gb/sec 
