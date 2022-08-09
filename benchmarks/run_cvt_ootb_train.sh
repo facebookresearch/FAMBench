153c30410562b14833a5b63508af68a746cc70a2
mindest <linminuser@gmail.com>
2022-08-09 18:28:52 +0800

    add benchmark for CvT/ootb

---
+++ benchmarks/run_cvt_ootb_train.sh
@@ -0,0 +1,39 @@
+NUM_GPUS=8
+task=train
+LOG_DIR=.
+
+usage() { echo "Usage: $0 [-g <NUM_GPUS>] [-t <'train'|'test'>]"; exit 1; }
+
+while getopts "g:t:h" flag
+do
+    case "${flag}" in
+        g) NUM_GPUS=${OPTARG} ;;
+        t) task=${OPTARG} ;;
+        h) usage
+    esac
+done
+shift $((OPTIND-1))
+
+benchmark=cvt
+implementation=ubench
+mode=${task}
+# LOGGER_FILE="${LOG_DIR}/${benchmark}_${implementation}_${mode}_g${NUM_GPUS}.log"
+
+echo "=== Launching FB5 ==="
+echo "Benchmark: ${benchmark}"
+echo "Implementation: ${implementation}"
+echo "Mode: ${mode}"
+# echo "Saving FB5 Logger File: ${LOGGER_FILE}"
+echo
+echo "Running Command:"
+
+(
+    set -x;
+    cd cvt/ootb;
+    bash prep_env_data.sh;
+    cd CvT;
+    bash run-alt.sh -g ${NUM_GPUS} -t ${task} --cfg experiments/imagenet/cvt/cvt-13-224x224.yaml \
+        DATASET.ROOT /tmp/DATASET/imagenet OUTPUT_DIR /tmp/OUTPUT
+)
+
+echo "=== Completed Run ==="
